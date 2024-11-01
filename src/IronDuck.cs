4cb2b3f2ac3b4225780e22f4d984b34cf4b32836
Gabriel Médici <142552114+medicegabriel@users.noreply.github.com>
2024-11-01 20:11:14 -0300

    Update IronDuck.cs

--- src/IronDuck.cs
+++ src/IronDuck.cs
@@ -1,1 +1,13 @@
+class RubberDuck : Duck, IQuackable
+{
+    public override void Display()
+    {
+        Console.WriteLine("Displaying a Iron Duck");
+    }
 
+
+    public void Quack()
+    {
+        Console.WriteLine("Quack, quack!!");
+    }
+}
